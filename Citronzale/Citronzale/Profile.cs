4571b63f3e1cd910e2c60274b03cf6aacebebdfb
kristzz <emils.krists@gmail.com>
2023-05-01 21:30:32 +0300

    create accounts with password only, login with pass

---
+++ Citronzale/Citronzale/Profile.cs
@@ -0,0 +1,21 @@
+using System;
+
+using LogSignIn;
+
+namespace Profile
+{
+    class PersonalProfile
+    {
+        
+        public void Main(string[] args)
+        {
+            var user = new PersonalProfile();
+            user.userInfo();
+        }
+
+        public void userInfo()
+        {
+
+        }
+    }
+}
--- Citronzale/Citronzale/SignLogIn.cs
+++ Citronzale/Citronzale/SignLogIn.cs
@@ -1,11 +1,23 @@
 using System;
 using System.Threading;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using Profile;
 
 namespace LogSignIn
 {
     class LogSign
     {
+        static List<string> lines;
+        static string creationPassword = "";
+        static string filepath = "pass.txt";
+
+        public string password;
+
         public static void Main(string[] args)
         {
             var starter = new LogSign();
@@ -21,9 +33,7 @@ namespace LogSignIn
             Console.WriteLine("We offer many great gym locations and trainers to train with!");
             Console.WriteLine("Choose what you want to do:");
 
-            options.Options();
-            
-            
+            options.Options(); 
         }
 
         public void Options()
@@ -33,9 +43,13 @@ namespace LogSignIn
             var exit = new LogSign();
             var options = new LogSign();
 
+            if (!File.Exists(filepath))
+                File.Create(filepath);
+            lines = new List<string>(File.ReadAllLines(filepath));
+
             Console.WriteLine("======================================================================");
-            Console.WriteLine("1. Log In to your profile");
-            Console.WriteLine("2. Sign Up and create a new profile");
+            Console.WriteLine("1. Sign Up and create a new profile");
+            Console.WriteLine("2. Log In to your profile");
             Console.WriteLine("3. Exit");
             Console.WriteLine("======================================================================");
             double input = double.Parse(Console.ReadLine());
@@ -44,10 +58,10 @@ namespace LogSignIn
             switch (input)
             {
                 case 1:
-                    login.LogIn();
+                    signup.SignUp();
                     break;
                 case 2:
-                    signup.SignUp();
+                    login.LogIn();
                     break;
                 case 3:
                     exit.Exit();
@@ -63,11 +77,48 @@ namespace LogSignIn
 
         public void LogIn()
         {
-            Console.WriteLine("LogIn");
+            var options = new LogSign();
+            var userProfile = new PersonalProfile();
+
+            Console.WriteLine("\nEnter Your Password");
+            password = Console.ReadLine();
+            if (userPass(password))
+            {
+                Console.WriteLine("You've successfully logged in.");
+                Console.WriteLine("Sveiki " + password);
+                userProfile.userInfo();
+            }
+            else
+            {
+                Console.WriteLine("Password not found");
+                Thread.Sleep(1500); // 1.5s sleep
+                Console.Clear();
+                options.Options();
+            }  
         }
+
+        static Boolean userPass(string psw)
+        {
+            foreach (var item in lines)
+            {
+                if (item == psw)
+                    return true;
+            }
+            return false; //japievieno lai parbauda ne tikai paroli bet ari username
+        }
+
         public void SignUp()
         {
-            Console.WriteLine("SignUp");
+            var options = new LogSign();
+
+            Console.WriteLine("Create your password:"); //japievieno lai var ari uztaisit username
+            creationPassword = Console.ReadLine();
+            lines.Add(creationPassword);
+            File.WriteAllLines(filepath, lines);
+            Console.WriteLine("You've successfully Signed Up.");
+            Thread.Sleep(1500); // 1.5s sleep
+            Console.Clear();
+            options.Options();
         }
         public void Exit()
         {
